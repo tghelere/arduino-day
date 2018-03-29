68a847712056195e5af79b31556024c6b1c03c6c
Thiago Paes <mrprompt@gmail.com>
2018-03-29 13:41:22 -0300

    fix(cibuild): Change path to build on cibuild

--- bin/cibuild.sh
+++ bin/cibuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec jekyll build
+bundle exec jekyll build -d _site/arduino-day
 bundle exec htmlproofer --allow-hash-href --disable-external --empty-alt-ignore ./_site
